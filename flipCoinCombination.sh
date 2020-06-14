68e3bcd717db6012766621403b7a443f8cc1534b
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-14 16:47:28 +0530

    UC-1 : find head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,12 @@
 
 echo "This problem displays winning percentage of head or tail combination in a Siglet, Doublet and Triplet"
 
+coin=$(( $RANDOM%2 ))
+
+if [ $coin -eq 0 ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
+
